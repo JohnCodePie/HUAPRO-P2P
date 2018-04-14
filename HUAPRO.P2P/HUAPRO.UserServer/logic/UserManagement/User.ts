428a2f5614f75f5f7d699a6b7a2d35ffc275ea66
Anton HUNDEGGER <hundegga@edu.htl-villach.at>
2018-04-14 10:21:28 +0200

    Did some UserManagement stuff.

---
+++ HUAPRO.P2P/HUAPRO.UserServer/logic/UserManagement/User.ts
@@ -0,0 +1,12 @@
+﻿class User {
+    private id: Number;
+    private username: string;
+    private eMail: string;      
+    private password: string; //later... has to be hashed
+
+    constructor(Username: string, EMail: string, Password: string) {
+        this.eMail = EMail;
+        this.username = Username;
+        this.password = Password;
+    }
+}
---
+++ HUAPRO.P2P/HUAPRO.UserServer/logic/UserManagement/UserManager.ts
@@ -0,0 +1,17 @@
+﻿class UserManager implements IManager {
+    //later... extend with mongoDB
+    private allUser: Array<User>;
+
+    public Create() {
+        throw new Error("Method not implemented.");
+    }
+    public Delete() {
+        throw new Error("Method not implemented.");
+    }
+    public Update() {
+        throw new Error("Method not implemented.");
+    }
+    public Get() {
+        throw new Error("Method not implemented.");
+    }
+}
